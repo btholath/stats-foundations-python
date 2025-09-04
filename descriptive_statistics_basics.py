fe1b4c11a64e7d732122a94070ec10e746d5732a
Biju Tholath <54602395+btholath@users.noreply.github.com>
2025-09-04 23:04:17 +0000

    stats

--- descriptive_statistics_basics.py
+++ descriptive_statistics_basics.py
@@ -5,3 +5,17 @@ data = [1,2, 3, 3]
 # Mean calculation
 mean = st.mean(data)
 print("Mean =", mean)
+
+
+# Median
+median = st.median(data)
+print("Mean =", mean)
+
+# standard deviation
+std_dev = st.stdev(data)
+print("Standard Deviation =", std_dev)
+
+# variance
+variance = st.variance(data)
+print("Variance =", variance)
+
